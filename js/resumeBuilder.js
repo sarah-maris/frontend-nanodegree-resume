b439513fa59d2421db7b716948fc73ee623e1c28
sarah_maris <sarah.87.maris@gmail.com>
2015-06-05 17:53:36 -0400

    Add jobs to resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 var bio = {
 		name : 'Sarah Maris',
 		role : 'Web Developer',
-		contacts :{
+		contacts : {
 			mobile : '908-902-8430',
 			email : 'sarah@sarahmaris.com',
 			twitter : '@sarah_l_maris',
@@ -11,21 +11,67 @@ var bio = {
 		bioPic : 'images/sarah-maris-sq.jpg',
 		welcomeMsg : 'I am a Web Developer focused on helping small businesses and non-profit organizations make the most of the free or near-free marketing and promotional tools available on the web.  I also provide training to help new or infrequent computer users and technophobes get the most out of their devices.',
 		skills : ['HTML/CSS', 'WordPress', 'Javascript', 'MailChimp', 'Facebook', 'Twitter', 'Small Business Marketing', 'Non-profit communications']
-};
+    };
 
 var work = {
-	workTitle : 'Web Developer',
-	workEmployer : 'Sarah\'s Small Business Solutions',
-	workYears : 5,
-	workCity : 'Fair Haven, NJ'
+	"jobs" : [
+		{
+			title : 'Web Developer/Small Business Consultant',
+			employer : 'Sarah\'s Small Business Solutions',
+			dates : "2009 - present",
+			location : 'Fair Haven, NJ',
+			description : 'Assist small business owners and non-profit organizations with website development, online marketing and computer training'
+		},
+		{
+			title : 'Assistant Brand Manager',
+			employer : 'The Quaker Oats Company',
+			dates : "1989-1992",
+			location : 'Chicago, IL',
+			description : 'Developed strategies to relaunch Cycle dog food and new child-focused oatmeal projects.  Planned and executed consumer and supply chain marketing efforts.  Collaborated  with a diverse team of internal and external partners, including manufacturing,   logistics, R&D, package design and advertising'
+		 }, 
+		 {
+			workTitle : 'Research Associate',
+			workEmployer : 'Booz-Allen & Hamilton',
+			workYears : "1985-1987",
+			workCity : 'Chicago, IL',
+			description : 'Worked with a team to advise organizations in the oilfield drilling equipment, professional beauty supply and banking industry on strategic issues including developing competitive advantage, improving efficiency and managing transitions.'
+		 }
+	],
+	"volunteerPositions" : [
+	
+	
+	
+	
+	
+	]
+};
+
+var education = {
+	"schools" : [
+	{
+		"name" : "Stanford University",
+		"city" : "Stanford, CA",
+		"degree" : "MBA",
+		"gradYear" : "1989"		
+	},
+	{
+		"name" : "Carleton College",
+		"city" : "Northfield, MN",
+		"degree" : "BA", 
+		"majors" : ["Economics"],
+		"gradYear" : "1985"	
+	}
+	],
+	"onlineCourses" : [
+	{
+		"name" : "Udacity",
+		"title" : "Front End Web Development Nanodegree",
+		"gradYear" : "2015"	
+	}
+	]
 }
 
-var education = new Object();
-education['name'] = 'Stanford University';
-education['years'] = '1987-1989';
-education['location'] = 'Stanford, CA';
-
-
+/*
 HTMLheaderName = HTMLheaderName.replace('%data%', bio.name);
 HTMLheaderRole = HTMLheaderRole.replace('%data%', bio.role);
 HTMLmobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
@@ -49,3 +95,4 @@ $('#topContacts').prepend(HTMLgithub);
 $('#topContacts').prepend(HTMLlocation);
 $('#workExperience').append(work.workTitle);
 $('#education').append(education['name']);
+*/
